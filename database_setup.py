9279e0046aee18841dc1654ff8ce564ec8dccb81
Evgeniya Bautina <janebarinova@gmail.com>
2015-11-18 16:07:54 -0800

    Add Goals, PhotoGoals, PhotoItems tables to the database

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 import sys
 import datetime
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime, BLOB
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -22,6 +22,25 @@ class Items(Base):
     savings_id = Column(Integer, ForeignKey('savings.id'))
     savings = relationship(Savings)
 
+class PhotoItems(Base):
+    __tablename__ = 'photoitems'
+    id = Column(Integer, primary_key = True)
+    filename = Column(String, nullable = False)
+    items_id = Column(Integer, ForeignKey('items.id'))
+    items = relationship(Items)
+
+class Goals(Base):
+    __tablename__ = 'goals'
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    price = Column(Float(presicion=2), default = 0)
+
+class PhotoGoals(Base):
+    __tablename__ = 'photogoals'
+    id = Column(Integer, primary_key = True)
+    filename = Column(String, nullable = False)
+    items_id = Column(Integer, ForeignKey('goals.id'))
+    items = relationship(Goals)
+
 engine = create_engine('sqlite:///savemoney.db')
-# Initialize database schema (create tables)
 Base.metadata.create_all(engine)
